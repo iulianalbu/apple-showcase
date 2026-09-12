2b7a4afc167a8effbeadbceac8d1a92f1d8b6b60
Iulian Albu <32983033+iulianalbu@users.noreply.github.com>
2026-09-12 12:41:38 +0300

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Iulian Albu on 12/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
